a889981f0731d9249749d186b1b4bf3ced137a9c
Justin Uzoije <justin.uzoije@searchdiscovery.com>
2019-05-07 12:45:10 -0400

    Add spawn child process example

---
+++ spawn.js
@@ -0,0 +1,42 @@
+
+const { spawn } = require('child_process');
+const readline = require('readline');
+
+async function sh(path, preserveGit, exclude) {
+  return new Promise(function (resolve, reject) {
+    const find = spawn('find', [path, '-type', 'f']);
+
+    let result = find
+    if (preserveGit) {
+      const grepGit = spawn(
+        'grep',
+        ['-v', '.git'],
+        { stdio: [result.stdout, 'pipe', process.error] },
+      );
+      result = grepGit
+    }
+
+    if (exclude !== null) {
+      const grepExclude = spawn(
+        'grep',
+        ['-v', exclude],
+        { stdio: [result.stdout, 'pipe', process.error] },
+      );
+      result = grepExclude
+    }
+
+    let lines = [];
+    const rl = readline.createInterface({ input: result.stdout });
+    rl.on('line', function(line) {
+      lines.push(line);
+    });
+
+    result.on('exit', function(data) {
+      resolve({ stdout: lines })
+    });
+  });
+}
+
+module.exports = {
+  sh,
+};
